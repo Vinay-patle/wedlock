d439af4a90c855683106963b13acb65dac055f67
Vinay patle <115407891+Vinay-patle@users.noreply.github.com>
2024-05-07 15:43:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.header').height($(window).height());
+  });
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+  social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+  social_panel_container.classList.remove('visible')
+});
